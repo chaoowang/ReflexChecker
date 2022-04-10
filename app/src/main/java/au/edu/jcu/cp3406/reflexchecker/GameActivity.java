c12e7dd8dcdc419c3066f005675d68a301cec1c7
chaoowang <chaohsuaan@gmail.com>
2022-04-10 15:21:04 +0800

    Create the game UI

--- app/src/main/java/au/edu/jcu/cp3406/reflexchecker/GameActivity.java
+++ app/src/main/java/au/edu/jcu/cp3406/reflexchecker/GameActivity.java
@@ -3,14 +3,51 @@ package au.edu.jcu.cp3406.reflexchecker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.Random;
 
 public class GameActivity extends AppCompatActivity {
 
     public static int GAME_REQUEST = 12345;
+    private final Random random = new Random();
+    private static final int[] drawables = {
+            R.drawable.drink_icon,
+            R.drawable.food_icon,
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
+
+        setupDescription(R.id.task1, R.array.task1_descriptions);
+        setupDescription(R.id.task2, R.array.task2_descriptions);
+        for (int i = 0; i < 5; i++) {
+            addImage();
+        }
     }
+
+    private void setupDescription(int taskID, int arrayID) {
+        TextView task = findViewById(taskID);
+        String[] descriptions = getResources().getStringArray(arrayID);
+
+        int i = random.nextInt(descriptions.length);
+        task.setText(descriptions[i]);
+    }
+
+    private void addImage() {
+        ViewGroup gameRows = findViewById(R.id.game_rows);
+        getLayoutInflater().inflate(R.layout.image, gameRows);
+
+        View lastChild = gameRows.getChildAt(gameRows.getChildCount()-1);
+        ImageView image = lastChild.findViewById(R.id.image);
+
+        int index = random.nextInt(drawables.length);
+        image.setImageDrawable(getResources().getDrawableForDensity(drawables[index], 0));
+    }
+
 }
